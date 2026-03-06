fcaceb6a5e748af8ee39cdd130d4ac6b979d64ae
mamad <137838327+rootmamad@users.noreply.github.com>
2026-03-07 03:20:32 +0330

    solve some problems with idor

--- main.py
+++ main.py
@@ -162,7 +162,10 @@ async def return_item(item_id:int , quantity:int, session:Session = Depends(get_
 
 @app.get("/order/{order_id}",response_model=OrderResponse)
 async def get_order(order_id:int,session:Session = Depends(get_session),current_user=Depends(access)) -> OrderResponse:
-    order = session.query(UserItem).get(order_id)
+    order = session.query(UserItem).filter(
+    UserItem.id == order_id, 
+    UserItem.user_id == current_user["user_id"]
+            ).first()
     if not  order:  
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="سفارش موجود نیست.")
     return {"username": order.user.username, "item": order.item.name, "quantity": order.quantity}
